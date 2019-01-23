4355b4ee6e492b7f154ac1890cc0a71cb7ccdd98
hiulit <hiulit@gmail.com>
2019-01-23 17:23:53 +0100

    Fix moving platforms colliding with themselves

--- Scripts/MovingPlatform.gd
+++ Scripts/MovingPlatform.gd
@@ -3,16 +3,20 @@ extends Node2D
 export var motion = Vector2()
 export var cycle = 1.0
 var accum = 0.0
+var direction = 1
 
 func _ready():
-	queue_free()
-	$KinematicBody2D.add_to_group("moving_platform")
+#	queue_free()
+	$Area2D.add_to_group("moving_platform")
 
 
 func _physics_process(delta):
-	accum += delta * (1.0 / cycle) * PI * 2.0
+	accum += delta * (1.0 / cycle) * PI * 2.0 * direction
 	accum = fmod(accum, PI * 2.0)
 	var d = sin(accum)
 	var xf = Transform2D()
 	xf[2]= motion * d
-	$KinematicBody2D.transform = xf
+	$Area2D.transform = xf
+
+func _on_Area2D_area_entered(area):
+	direction *= -1
